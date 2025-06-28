be2f27cf7431203aaca8cfb426cf9d89fd3a2c22
Shivam Gupta <shivamgupta35967@gmail.com>
2025-06-28 12:15:44 +0530

    fixed reloading issue

--- client/src/components/ProtectedRoutes.jsx
+++ client/src/components/ProtectedRoutes.jsx
@@ -2,7 +2,12 @@ import { Outlet, Navigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
 const ProtectedRoutes = () => {
-  const { loggedIn } = useAuth();
+  const { loggedIn, authLoading } = useAuth();
+
+  if (authLoading) {
+    return <div className="p-8 text-center">Checking login status...</div>;
+  }
+
   return loggedIn ? <Outlet /> : <Navigate to="/login" />;
 };
 
