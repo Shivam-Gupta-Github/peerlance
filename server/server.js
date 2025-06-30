130cda8d7222c22d612ede40720afdf9abd83a21
Shivam Gupta <shivamgupta35967@gmail.com>
2025-06-30 13:05:38 +0530

    fixed server.js

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ import jobRoutes from "./routes/job.js";
 import applicationRoutes from "./routes/application.js";
 
 const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
 
 dotenv.config();
 
@@ -16,7 +17,7 @@ app.use(express.json());
 app.use(cors());
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
@@ -24,6 +25,6 @@ app.use("/api/auth", authRouter);
 app.use("/api/jobs", jobRoutes);
 app.use("/api/applications", applicationRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
